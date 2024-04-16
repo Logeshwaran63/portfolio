234199aa6b3363e1f93830491e51eeaadc52eb80
logeshwaran96 <vlogeshwaran77@gmail.com>
2024-04-16 15:56:13 +0530

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/aboutlogo.jpg"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Logeshwaran</strong>. I'm from Tanjore and have been living in Kumbakonam for the past 12 years. During my time here, I gained valuable experience through various entry-level positions, which greatly enhanced my work ethic, communication skills, and adaptability.
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+            From a young age, I've been captivated by the endless possibilities of the web and the power of code to bring ideas to life. What started as a hobby quickly evolved into a dedicated pursuit, driving me to delve into the intricacies of front-end and back-end development.
+            As a recent graduate in Computer science Engineering at SJCET College , I've eagerly embraced every opportunity to expand my skills and stay current with emerging technologies. My journey so far has been marked by a series of exciting projects where I've tackled challenges head-on, honing my abilities in HTML, CSS, JavaScript, and more.
+</ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and cross-platform mobile development.
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
